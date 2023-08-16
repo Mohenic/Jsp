9b44877fba597def6f0788ec726dcddea4051f65
chhak0503 <chhak0503@gmail.com>
2023-08-16 12:27:19 +0900

    팜스토리 실습

--- Farmstory1/src/main/java/kr/farmstory1/dao/UserDAO.java
+++ Farmstory1/src/main/java/kr/farmstory1/dao/UserDAO.java
@@ -15,9 +15,27 @@ public class UserDAO extends DBHelper {
 
 	// 기본 CRUD 
 	public void insertUser(UserDTO dto) {
-		
+		try {
+			conn = getConnection();
+			psmt = conn.prepareStatement(SQL.INSERT_USER);
+			psmt.setString(1, dto.getUid());
+			psmt.setString(2, dto.getPass());
+			psmt.setString(3, dto.getName());
+			psmt.setString(4, dto.getNick());
+			psmt.setString(5, dto.getEmail());
+			psmt.setString(6, dto.getHp());
+			psmt.setString(7, dto.getZip());
+			psmt.setString(8, dto.getAddr1());
+			psmt.setString(9, dto.getAddr2());
+			psmt.setString(10, dto.getRegip());
+			psmt.executeUpdate();
+			close();
+			
+		}catch(Exception e){
+			e.printStackTrace();
+		}
 	}
-	public UserDTO selectUser(String uid, String pass) {
+	public UserDTO selectUser() {
 		return null;
 	}
 	
@@ -25,7 +43,78 @@ public class UserDAO extends DBHelper {
 	public void updateUser() {}
 	public void deleteUser() {}
 	
-	// 추가 
+	// 추가
+	public int selectCountUid(String uid) {
+		int result = 0;
+		try{
+			conn = getConnection();
+			psmt = conn.prepareStatement(SQL.SELECT_COUNT_UID);
+			psmt.setString(1, uid);
+			rs = psmt.executeQuery();
+			
+			if(rs.next()){
+				result = rs.getInt(1);
+			}
+			close();
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		return result;
+	}
+	public int selectCountNick(String nick) {
+		int result = 0;
+		try{
+			conn = getConnection();
+			psmt = conn.prepareStatement(SQL.SELECT_COUNT_NICK);
+			psmt.setString(1, nick);
+			rs = psmt.executeQuery();
+			
+			if(rs.next()){
+				result = rs.getInt(1);
+			}
+			close();			
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		return result;
+	}
+	public int selectCountHp(String hp) {
+		int result = 0;
+		try{
+			conn = getConnection();
+			psmt = conn.prepareStatement(SQL.SELECT_COUNT_HP);
+			psmt.setString(1, hp);
+			rs = psmt.executeQuery();
+			
+			if(rs.next()){
+				result = rs.getInt(1);
+			}
+			
+			close();
+			
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		return result;
+	}
+	public int selectCountEmail(String email) {
+		int result = 0;
+		try{
+			conn = getConnection();
+			psmt = conn.prepareStatement(SQL.SELECT_COUNT_EMAIL);
+			psmt.setString(1, email);
+			rs = psmt.executeQuery();
+			
+			if(rs.next()){
+				result = rs.getInt(1);
+			}
+			close();
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		return result;
+	}
+	
 	public TermsDTO selectTerms() {
 		TermsDTO dto = new TermsDTO();
 
